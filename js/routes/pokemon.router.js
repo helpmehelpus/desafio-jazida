a89ac96286edc0e70b8105db80a074a0e4b03b5a
adriano <adrianortorres@gmail.com>
2020-06-16 17:00:10 -0300

    documented Pokemon POST

--- js/routes/pokemon.router.js
+++ js/routes/pokemon.router.js
@@ -5,6 +5,23 @@ const pokemons = new Pokemons();
 
 const router = express.Router();
 
+/**
+ * @api {POST} /pokemons [POST] Criar novo pokemon
+ * @apiGroup Pokemons
+ *
+ * @apiSuccess {JSON} Pokemon
+ * @apiSuccessExample {JSON} Success-Response:
+ *  HTTP/1.1 200 OK
+ *  {
+ *    "id": 11
+ *    "tipo": "charizard"
+ *    "treinador": "Ash"
+ *    "nivel": 1
+ *  }
+ * @apiErrorExample {JSON}
+ *  HTTP/1.1 400
+ *
+ */
 router.route('/')
   .post((req, res) => {
     pokemons.criar(req.body)
