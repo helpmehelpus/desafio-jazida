ab84c10516aa6f6f0e55b6d1fd6a80564fc66d89
adriano <adrianortorres@gmail.com>
2020-06-17 10:27:30 -0300

    corrigido batalhar()

--- js/controllers/batalhas.js
+++ js/controllers/batalhas.js
@@ -13,34 +13,29 @@ class Batalhas {
     if (!pokemonB.id) {
       return { code: 404, message: `Pokemon com id ${pokemonBId} nao encontrado.` };
     }
-    const pokemonAVenceu = false;
+    let vencedor;
+    let perdedor;
     const chanceVitoriaDeA = pokemonA.nivel / (pokemonA.nivel + pokemonB.nivel);
     const sorteio = Math.random();
     if (sorteio <= chanceVitoriaDeA) {
       console.log(`Pokemon ${pokemonA.id} vence`);
       pokemonA = await pokemons.incrementaNivel(pokemonA.id);
       pokemonB = await pokemons.decrementaNivel(pokemonB.id);
-      pokemonAVenceu = true;
+      vencedor = pokemonA;
+      perdedor = pokemonB;
     } else {
       console.log(`Pokemon ${pokemonB.id} vence`);
       pokemonB = await pokemons.incrementaNivel(pokemonB.id);
       pokemonA = await pokemons.decrementaNivel(pokemonA.id);
+      vencedor = pokemonB;
+      perdedor = pokemonA;
     }
-    let result;
-    if (pokemonAVenceu) {
-      result = {
-        vencedor: pokemonA,
-        perdedor: pokemonB,
-      };
-    } else {
-      result = {
-        vencedor: pokemonB,
-        perdedor: pokemonA,
-      };
-    }
-    console.log(`novo level pokemon ${pokemonA.id}:`, pokemonA.nivel);
-    console.log(`novo level pokemon ${pokemonB.id}:`, pokemonB.nivel);
-    return result;
+    console.log(vencedor);
+    console.log(perdedor);
+    return {
+      vencedor,
+      perdedor,
+    };
   }
 }
 
